c873a4c8acbdba11a4022c0d91d5f213b5e4e295
jengland <jengland@healthgrades.com>
2017-01-12 17:46:22 -0700

    adding the logging back

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ var phantomjs = require('phantomjs-prebuilt');
 exports.handler = function(event, context, callback) {
     var bucketName = process.env.S3_BUCKET;
 
-    var program = phantomjs.exec('phantomjs-script.js', 'arg1', 'arg2')
-    // program.stdout.pipe(process.stdout)
-    // program.stderr.pipe(process.stderr)
+    var phantom = phantomjs.exec('phantomjs-script.js', 'arg1', 'arg2')
+    phantom.stdout.pipe(process.stdout)
+    phantom.stderr.pipe(process.stderr)
 
-    program.on('exit', code => {
+    phantom.on('exit', code => {
         callback(null, bucketName);
     })
 
