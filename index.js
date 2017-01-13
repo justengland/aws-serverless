920fadee87ab1040ee84af52600d1e955d62dfc3
jengland <jengland@healthgrades.com>
2017-01-12 17:35:47 -0700

    adding phantomjs

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 
 var AWS = require('aws-sdk');
-
+var phantomjs = require('phantomjs-prebuilt');
 
 exports.handler = function(event, context, callback) {
     var bucketName = process.env.S3_BUCKET;
 
-    console.log('Ok I am logging for reals');
-    callback(null, bucketName);
+    var program = phantomjs.exec('phantomjs-script.js', 'arg1', 'arg2')
+    // program.stdout.pipe(process.stdout)
+    // program.stderr.pipe(process.stderr)
+
+    program.on('exit', code => {
+        callback(null, bucketName);
+    })
+
+
 }
