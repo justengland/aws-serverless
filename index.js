3baf9de5fc7ae53b7fb14cd0c1c9a80130512041
jengland <jengland@healthgrades.com>
2017-01-12 19:09:38 -0700

    adding phantom back in

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 
-// var AWS = require('aws-sdk');
-// var phantomjs = require('phantomjs-prebuilt');
+var AWS = require('aws-sdk');
+var phantomjs = require('phantomjs-prebuilt');
 
 exports.handler = function(event, context, callback) {
     console.log('The monk has landed');
     callback(null, "fin!");
 
-    // var phantom = phantomjs.exec('phantomjs-script.js', 'arg1', 'arg2');
-    //
-    // phantom.stdout.on('data', function(buf) {
-    //     console.log('[STR] stdout "%s"', String(buf));
-    // });
-    // phantom.stderr.on('data', function(buf) {
-    //     console.log('[STR] stderr "%s"', String(buf));
-    // });
-    // phantom.on('close', function(code) {
-    //     console.log('[END] code', code);
-    // });
-    //
-    // phantom.on('exit', code => {
-    //     callback(null, bucketName);
-    // });
+    var phantom = phantomjs.exec('phantomjs-script.js', 'arg1', 'arg2');
+
+    phantom.stdout.on('data', function(buf) {
+        console.log('[STR] stdout "%s"', String(buf));
+    });
+    phantom.stderr.on('data', function(buf) {
+        console.log('[STR] stderr "%s"', String(buf));
+    });
+    phantom.on('close', function(code) {
+        console.log('[END] code', code);
+    });
+
+    phantom.on('exit', code => {
+        callback(null, bucketName);
+    });
 
 };
